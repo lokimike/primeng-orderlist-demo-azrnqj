e4618c1e5f5feedd92b99cf596562927f0805dac
lokimike <lokmike626@gmail.com>
2021-05-24 14:24:47 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductService } from './productservice';
 import { Product } from './product';
 import { PrimeNGConfig } from 'primeng/api';
@@ -8,13 +8,29 @@ import { PrimeNGConfig } from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { 
-    products: Product[];
-    
-    constructor(private productService: ProductService, private primengConfig: PrimeNGConfig) { }
+export class AppComponent {
+  products: Product[];
+  status: string[] = ['Order Received', 'Preparing', 'Ready To Serve'];
+  currentStatus: string = 'Order Received';
+  constructor(
+    private productService: ProductService,
+    private primengConfig: PrimeNGConfig
+  ) {}
 
-    ngOnInit() {
-        this.productService.getProductsSmall().then(order => this.products = order);
-        this.primengConfig.ripple = true;
+  ngOnInit() {
+    this.productService
+      .getProductsSmall()
+      .then(order => (this.products = order));
+    this.primengConfig.ripple = true;
+  }
+
+  changeStatus(status) {
+    console.log(status);
+    if (status == this.status[0]) {
+      this.currentStatus = this.status[1];
     }
+    if (status == this.status[1]) {
+      this.currentStatus = this.status[2];
+    }
+  }
 }
